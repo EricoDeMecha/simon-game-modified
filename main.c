1039403bdbead964e6b33b85d3acb45a514275d6
EricoDeMecha <techcider4134@gmail.com>
2020-12-07 05:54:32 +0300

    added discrete values for delay

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include "daqsim.h"
 
+#define LEDs 4
+
 // random numbers
 int randInt(int lower, int upper){
     // pseudo-random integer generator
@@ -18,7 +20,26 @@ void generateSequence(int length, int data[]){
         data[i] = randInt(0,3); // 0-3 to represent the indexes of the leds
     }
 }
+void resetLEDs(){
+    // reset leds
+    for (int j = 0; j < LEDs; ++j) {
+        digitalWrite(j, 0);
+    }
+}
 
+void waitForClick(int clicked_button_st, int  pin){
+    // set the end time to the current time plus 5 seconds
+    time_t endTime = time(NULL) + 5;
+    int clicked_button_st_rt = digitalRead(pin); // read from odd channels
+    while (time(NULL) < endTime)
+    {
+        clicked_button_st = digitalRead(pin); // read from odd channels
+        if(clicked_button_st != clicked_button_st_rt){
+            break;
+        }
+    }
+    // end of loop waiting for results
+}
 int main(void) {
     // seed the generator  should only be called once
     srand((unsigned)time(0));
@@ -30,33 +51,40 @@ int main(void) {
 	//Main loop to maintain the gpu
 	while (continueSuperLoop()) {
 		// Sleep to avoid high CPU usage.
-		daq_sleep(10);
+		daq_sleep(1000);
 		// generate sequence
 		int data[s_counter];
 		generateSequence(s_counter, data);
 		// display the  sequence
 		for(size_t i = 0; i < s_counter; i++){
 		    digitalWrite(data[i], 1); // flash the LEDs in a sequence one by one
-		    daq_sleep(5);
+		    daq_sleep(500);
             digitalWrite(data[i], 0); // flash the LEDs in a sequence one by one
-            daq_sleep(5);
+            daq_sleep(500);
 		}
+		// reset leds
+		resetLEDs();
 		// read the buttons
         // modify the array to include the real locations
         for(size_t i = 0; i < s_counter; i++){
             data[i] = (data[i] + 1); // changing the values to actual locations
         }
+
         // process clicks
         bool  fail_check = false;
 		for(size_t i = 0; i < s_counter; i++){
 		    if(data[i] % 2 != 0){ // odd
-                int  clicked_button_st = digitalRead((data[i] - 1 )); // read from odd channels
+		        int clicked_button_st = 0;
+                int btn = (data[i] - 1 );
+                waitForClick((int) &clicked_button_st, btn);
                 if(clicked_button_st == 0){
                     fail_check = false;
                 }
 		    }else{
-                int  clicked_button_st = digitalRead((data[i] - 1 )); // read from even channels
-                if (clicked_button_st == 1){
+                int clicked_button_st2 = 0;
+		        int btn2 = (data[i] - 1 );
+		        waitForClick((int) &clicked_button_st2, btn2);
+                if (clicked_button_st2 == 1){
                     fail_check = true;
                 }
 		    }
@@ -64,24 +92,22 @@ int main(void) {
 		if(!fail_check){ // success blink blue 3 times
 		    for( int i = 0; i < 3; i++){
 		        digitalWrite(3, 1);
-		        daq_sleep(5);
+		        daq_sleep(500);
                 digitalWrite(3, 0);
-                daq_sleep(5);
+                daq_sleep(500);
 		    }
 		    s_counter = (s_counter++ <= 5) ? ( s_counter++ ): 1 ; // else start the game
 		}else{ // failure blinks read blue times
             for( int i = 0; i < 3; i++){
-                digitalWrite(1, 1);
-                daq_sleep(5);
-                digitalWrite(1, 0);
-                daq_sleep(5);
+                digitalWrite(2, 1);
+                daq_sleep(500);
+                digitalWrite(2, 0);
+                daq_sleep(500);
             }
             // s_counter retains the original value
 		}
-		// resets all the leds before next loop
-        for (int j = 0; j < 4; ++j) {
-            digitalWrite(j, 0);
-        }
+        // resets all the leds before next loop
+        resetLEDs();
 	}
 	return 0;
 };
